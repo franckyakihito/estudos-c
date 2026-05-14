6472bcb9aa99aa746a7749ce37297e3a13475296
Francky Akihito <franckyakihito@gmail.com>
2026-05-14 17:28:09 -0300

    corrige typo no variaveis.c

--- variaveis.c
+++ variaveis.c
@@ -7,7 +7,7 @@ int main(){
     printf("Digite sua idade: "); scanf("%i", &idade);
     printf("Digite sua altura:"); scanf("%f", &altura);
     printf("Digite seu nome: "); scanf(" %[^\n]", nome); // o %[^\n] é um truque para ler os espaços no nome para o printf digitar o nome completo caso assim inserido.
-//o ^é o símbolo de negação e o /n é caractere de nova linha, o enter, ou seja, ele le todos os caracteres digitados exceto o enter
+//o ^ é o símbolo de negação e o /n é caractere de nova linha, o enter, ou seja, ele le todos os caracteres digitados exceto o enter
 // tambem precisa dar um espaço no scanf do nome para evitar que o enter do scanf anterior seja lido como parte do nome 
 printf("Ola %s  , voce tem %i anos e sua altura e %.2f metros", nome, idade, altura);
     
